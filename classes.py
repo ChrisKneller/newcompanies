28277a796dbd3e103581c4be09a9cdfe29be07cc
Chris <christian.kneller@gmail.com>
2020-05-05 12:32:55 +0100

    Created sqlalchemy classes for db tables

---
+++ classes.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Sequence
+
+Base = declarative_base()
+
+class Company(Base):
+     __tablename__ = 'companies'
+
+     id = Column(Integer, Sequence('user_id_seq'),)
+     number = Column(Integer, primary_key=True)
+     name = Column(String)
+     incorporated_on = Column(Date)
+
+     def __repr__(self):
+        return f"<Company(company='{self.name}', number={self.number}, incorporated_on={self.incorporated_on})>" 
+
+
+class Test(Base):
+    __tablename__ = 'testtable'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    testfield = Column(String)
+
+    def __repr__(self):
+        return f"<Test(id='{self.id}', testfield={self.testfield})>" 
