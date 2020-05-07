92252e1efae6e5e28e56839fb91ec67761416c8f
Chris <christian.kneller@gmail.com>
2020-05-07 07:42:56 +0100

    Finished up initial classes

--- classes.py
+++ classes.py
@@ -4,15 +4,17 @@ from sqlalchemy import Column, Integer, String, Date, Sequence
 Base = declarative_base()
 
 class Company(Base):
-     __tablename__ = 'companies'
+    __tablename__ = 'companies'
 
-     number = Column(Integer, primary_key=True)
-     name = Column(String)
-     incorporated_on = Column(Date)
+    number = Column(Integer, primary_key=True)
+    name = Column(String)
+    incorporated_on = Column(Date)
 
-     def __repr__(self):
-        return f"<Company(company='{self.name}', number={self.number}, incorporated_on={self.incorporated_on})>" 
+    def as_json(self):
+       return {"number": self.number, "name": self.name, "incorporated_on": str(self.incorporated_on.isoformat())}
 
+    def __repr__(self):
+        return f"<Company(number='{self.number}', name='{self.name}', incorporated_on='{self.incorporated_on.isoformat}')>"
 
 class Test(Base):
     __tablename__ = 'testtable'
